db0b42b269df99488887a987519710b4f5b6f2e4
Mazen Sehgal <mazen_sehgal@hotmail.com>
2017-02-01 08:54:25 +0000

    fixing my stuff

--- pt/content/footer/scripts.php
+++
@@ -1,8 +0,0 @@
-<?php 
-// if accessed directly than exit
-if (!defined('ABSPATH')) exit;
-?>
-
-<!-- Custom Theme Scripts -->
-<script src="<?php echo JS_URL;?>custom.js"></script>
-<script src="<?php echo JS_URL;?>styles.js"></script>
--- pt/inc/process.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-//Load all functions
-require_once('../load.php');
-require_once( ABSPATH . CONTENT . '/process/process.php');
-?>
