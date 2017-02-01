2fbf7cce083e2287db9b0e0925661d6f4966f629
Mark Halling-Brown <markyhb@gmail.com>
2017-02-01 09:13:33 +0000

    Altering config file

--- pt/config.php
+++ pt/config.php
@@ -1,11 +1,25 @@
 <?php
-define('DB_NAME', 'randomise');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
-define('DB_HOST', 'localhost');
+$db_name = 'randomise';
+$db_user = 'root';
+$db_password = '';
+$db_host = 'localhost';
+$webpath = 'WebProjects/randomisation/pt';
+if($_SERVER['SERVER_ADDR'] == '10.161.146.74' || $_SERVER['SERVER_ADDR'] == '10.161.128.46') {
+	$db_user = 'markyhb';
+	$db_password = 'ore28gon';
+	$db_host = '10.161.128.194';
+	$webpath = 'WebProjects/randomisation/randomisation_website/pt';
+	if($_SERVER['SERVER_ADDR'] == '10.161.146.74') {
+		$webpath = 'trialRandomisation';
+	}
+}
+define('DB_NAME', $db_name);
+define('DB_USER', $db_user);
+define('DB_PASSWORD', $db_password);
+define('DB_HOST', $db_host);
 define( 'INC', 'inc' );
 define( 'CONTENT', 'content' );
-define('WEBPATH','WebProjects/randomisation/pt');
+define('WEBPATH', $webpath);
 
 if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
